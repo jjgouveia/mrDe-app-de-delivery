97ad5ab85c360c0e1535139c716de4832b627075
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-20 18:01:21 -0300

    Criação do arquivo de rotas

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const routes = require('../routes/routes');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(routes)
 
 module.exports = app;
