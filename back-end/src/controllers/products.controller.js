bc73fde7ae948af87d3e8a6ae81bf884cab7224c
cafe51 <cafecafe51@hotmail.com>
2023-01-24 14:30:37 -0400

    lint corrigido

--- back-end/src/controllers/products.controller.js
+++ back-end/src/controllers/products.controller.js
@@ -1,11 +1,9 @@
 const service = require('../services/products.service');
 
-const getAllProducts = async(req, res) => {
+const getAllProducts = async (req, res) => {
     const allProducts = await service.getAllProducts();
     return res.status(200).json(allProducts);
 };
-
-
 
 module.exports = {
     getAllProducts };
--- back-end/src/services/products.service.js
+++ back-end/src/services/products.service.js
@@ -3,8 +3,7 @@ const { product } = require('../database/models');
 const getAllProducts = async () => {
     const allProducts = await product.findAll();
     return allProducts;
-}
-
+};
 
 module.exports = {
     getAllProducts,
