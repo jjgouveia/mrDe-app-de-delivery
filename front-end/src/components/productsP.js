c0aa30f9ce207ac006f162af7e88ad214c872f07
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-31 19:48:01 -0300

    build: Req 24

--- front-end/src/components/productsP.js
+++ front-end/src/components/productsP.js
@@ -1,38 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function ProductP(props) {
   const { product } = props;
   const { id, status, saleDate, totalPrice } = product;
   return (
-    <div>
-      <p>Pedido: </p>
-      <span
-        data-testid={ `customer_orders__element-order-id-${id}` }
-      >
-        { id }
-      </span>
+    <div style={ { border: '1px solid red', padding: '1' } }>
+      <Link to={ `${id}` }>
+        <div>
+          <p>
+            Pedido:
+            <span
+              data-testid={ `customer_orders__element-order-id-${id}` }
+            >
+              { id }
+            </span>
+          </p>
+        </div>
 
-      <p>Status: </p>
-      <span
-        data-testid={ `customer_orders__element-delivery-status-${id}` }
-      >
-        { status }
-      </span>
+        <p>
+          Status:
+          <span
+            data-testid={ `customer_orders__element-delivery-status-${id}` }
+          >
+            { status }
+          </span>
+        </p>
 
-      <p>Data: </p>
-      <span
-        data-testid={ `customer_orders__element-order-date-${id}` }
-      >
-        { saleDate }
-      </span>
+        <p>
+          Data:
+          <span
+            data-testid={ `customer_orders__element-order-date-${id}` }
+          >
+            { saleDate }
+          </span>
+        </p>
 
-      <p>Preço:</p>
-      <span
-        data-testid={ `customer_orders__element-card-price--${id}` }
-      >
-        { totalPrice }
-      </span>
+        <p>
+          Preço:
+          <span
+            data-testid={ `customer_orders__element-card-price--${id}` }
+          >
+            { totalPrice }
+          </span>
+        </p>
+      </Link>
     </div>
   );
 }
