43a6b5fb990adf85e20202efd0c13b174d3e342e
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2023-02-06 15:42:00 -0300

    4

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -5,6 +5,7 @@ import NavBar from '../components/navbar';
 import OrderProducts from '../components/OrderProducts';
 import { registerSale } from '../routes/order.routes';
 import AppContext from '../context/app.context';
+import '../css/Checkout.css';
 
 const STATUS_CREATED = 201;
 
@@ -50,10 +51,41 @@ function Checkout() {
   return (
     <div>
       <NavBar />
-      <h1>
-        Finalizar pedido
-      </h1>
-      <div>
+      <div className="cards-orders">
+        <h1 className="title-order">
+          Revisar pedido -
+          { ' ' }
+          <span>
+            Total: R$
+            { ' ' }
+            <span data-testid="customer_checkout__element-order-total-price">
+              {totalPrice.replace(/\./, ',')}
+            </span>
+          </span>
+        </h1>
+        <table className="header-table">
+          <tr
+            className="card-order"
+            // style={ {
+            //   padding: ,
+            // } }
+          >
+            <th className="elem-order aux3">Item</th>
+            <th
+              className="elem-order diferente aux2"
+              style={ {
+                backgroundColor: 'rgb(158, 79, 238)',
+                color: 'white',
+              } }
+            >
+              Descrição
+            </th>
+            <th className="elem-order aux3">Qnt</th>
+            <th className="elem-order aux3">Unidade</th>
+            <th className="elem-order aux3">Total</th>
+            <th className="elem-order aux3">rem</th>
+          </tr>
+        </table>
         {cart.map((checkoutOrder) => (
           <OrderProducts
             product={ checkoutOrder }
@@ -62,53 +94,50 @@ function Checkout() {
           />
         ))}
       </div>
-      <div>
-        <strong>Total: </strong>
-        <span>R$ </span>
-        {' '}
-        <span data-testid="customer_checkout__element-order-total-price">
-          {totalPrice.replace(/\./, ',')}
-        </span>
-      </div>
-      <form onSubmit={ handleSubmit(onSubmit) }>
-        <h1>
-          Detalhes de Endereço para Entrega
-        </h1>
-        <div>
-          <p>
-            P. Vendedora Responsável:
-          </p>
-          <select
-            data-testid="customer_checkout__select-seller"
-            name="select"
-            { ...register('sellerId') }
-          >
-            { sellers?.map(({ name, id }) => (
-              <option key={ id } value={ id }>{name}</option>))}
-          </select>
+      <form className="field" onSubmit={ handleSubmit(onSubmit) }>
+        <h1 className="legenda">Detalhes de Endereço para Entrega</h1>
+        <div className="cont-field">
+          <div className="cont-field-elems">
+            <p className="sub-titles">
+              P. Vendedora Responsável:
+            </p>
+            <select
+              data-testid="customer_checkout__select-seller"
+              name="select"
+              { ...register('sellerId') }
+            >
+              { sellers?.map(({ name, id }) => (
+                <option key={ id } value={ id }>{name}</option>))}
+            </select>
+          </div>
+          <div className="cont-field-elems input-diferente">
+            <p className="sub-titles">
+              Endereço
+            </p>
+            <input
+              className="input-diferente"
+              placeholder="Travessa Terceira..."
+              data-testid="customer_checkout__input-address"
+              type="text"
+              { ...register('deliveryAddress') }
+            />
+          </div>
+          <div className="cont-field-elems outro-input">
+            <p className="sub-titles">
+              Número
+            </p>
+            <input
+              className="input-diferente outro-input"
+              placeholder="198"
+              data-testid="customer_checkout__input-address-number"
+              type="text"
+              { ...register('deliveryNumber') }
+            />
+          </div>
         </div>
-        <div>
-          <p>
-            Endereço
-          </p>
-          <input
-            data-testid="customer_checkout__input-address"
-            type="text"
-            { ...register('deliveryAddress') }
-          />
-        </div>
-        <div>
-          <p>
-            Número
-          </p>
-          <input
-            data-testid="customer_checkout__input-address-number"
-            type="text"
-            { ...register('deliveryNumber') }
-          />
-        </div>
-        <div>
+        <div className="box-button">
           <button
+            className="btn-finalizar"
             data-testid="customer_checkout__button-submit-order"
             type="submit"
           >
